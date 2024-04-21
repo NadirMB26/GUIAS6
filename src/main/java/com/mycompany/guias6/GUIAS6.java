026922a59c4619f72b3736a24e5280e3a32ab6f2
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:55:35 -0500

    primer GUIA6

---
+++ src/main/java/com/mycompany/guias6/GUIAS6.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias6;
+
+import igui.pantallas6;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS6 {
+
+    public static void main(String[] args) {
+    new pantallas6().setVisible(true);
+    }
+}
